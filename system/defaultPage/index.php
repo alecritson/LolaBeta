af59862ded6bcf02efcd935a5afe611edf17a833
Alec Ritson <info@alecritson.co.uk>
2013-02-20 00:01:58 +0000

    Added some styling to defaultPage/index

--- system/defaultPage/index.php
+++ system/defaultPage/index.php
@@ -1,1 +1,15 @@
-Hello
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lola</title>
+	<style type="text/css">
+		body {background:#e4e4e4;}
+		#logo-holder {width:502px; height:245px;position: absolute;	left:50%; margin-left:-301px; top:50%; margin-top:-122px;}
+	</style>
+</head>
+
+<body>
+	<div id="logo-holder"><img src="http://alecritson.co.uk/assets/lola-logo.png"></div>
+</body>
+</html>
